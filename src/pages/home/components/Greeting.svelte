7023df8b9536fb0060b3e4e140c2b090699cb7d3
Illia Chaban <illia.chaban.8@gmail.com>
2021-08-11 23:54:51 -0400

    fix greeting

--- src/pages/home/components/Greeting.svelte
+++ src/pages/home/components/Greeting.svelte
@@ -1,91 +1,89 @@
 <script lang="ts">
-import { onMount } from "svelte";
+  import { onMount } from "svelte";
+  import anime from 'animejs'
+
+  const wrapEveryLetterInSpan = (selector: string) => {
+    let elements = document.querySelectorAll(selector);
+    elements.forEach(el => {
+      el.innerHTML = el.textContent?.replace(/(\w|,|')/g, "<span class='letter'>$&</span>") ?? "";
+    })
+  };
 
 
+  let animations: anime.AnimeTimelineInstance[] = [];
+  const prepareAnimations = () => {
 
-// const wrapEveryLetterInSpan = (selector) => {
-//     let elements = document.querySelectorAll(selector);
-//     elements.forEach(el => {
-//       el.innerHTML = el.textContent.replace(/(\w|,|')/g, "<span class='letter'>$&</span>");
-//     })
-//   };
-
-
-//   let animations = [];
-//   const prepareAnimations = () => {
-
-//     wrapEveryLetterInSpan('.ml3');
-//     document.querySelectorAll('.ml3 .letter').forEach( l => {
-//       l.style.opacity = 0;
-//     });
-//     let headerFadeIn = anime.timeline({loop: false})
-//       .add({
-//         targets: '.ml3 .letter',
-//         opacity: [0,1],
-//         easing: "easeInOutQuad",
-//         duration: 250,
-//         delay: function(el, i) {
-//           return 50 * (i+1)
-//         }
-//       });
+    wrapEveryLetterInSpan('.ml3');
+    document.querySelectorAll('.ml3 .letter').forEach( l => {
+      (l as any).style.opacity = 0;
+    });
+    let headerFadeIn = anime.timeline({loop: false})
+      .add({
+        targets: '.ml3 .letter',
+        opacity: [0,1],
+        easing: "easeInOutQuad",
+        duration: 250,
+        delay: function(el, i) {
+          return 50 * (i+1)
+        }
+      });
 
   
-//     // HI
-//     // document.querySelector(".ml8").classList.remove('hide');
-//     let hiAnimation = anime.timeline({loop: false}).add({
-//       targets: '.ml8 .circle-white',
-//       scale: [0, 3],
-//       opacity: [1, 0],
-//       easing: "easeInOutExpo",
-//       rotateZ: 360,
-//       duration: 1100
-//     })
-//     .add({
-//       targets: '.ml8 .circle-container',
-//       scale: [0, 1],
-//       duration: 1100,
-//       easing: "easeInOutExpo",
-//       offset: '-=1000'
-//     })
-//     .add({
-//       targets: '.ml8 .circle-dark',
-//       scale: [0, 1],
-//       duration: 1100,
-//       easing: "easeOutExpo",
-//       offset: '-=600'
-//     })
-//     .add({
-//       targets: '.ml8 .letters-left',
-//       scale: [0, 1],
-//       translateY: ['38%', '38%'], //centering letters
-//       duration: 1200,
-//       offset: '-=550'
-//     })
-//     .add({
-//       targets: '.ml8 .bang',
-//       scale: [0, 1],
-//       translateY: ['38%', '38%'], //centering letters
-//       opacity: [0,1],
-//       rotateZ: [45, 15],
-//       duration: 1200,
-//       offset: '-=1000'
-//     })
-//     .add({}); // fixes "!" bug
+    // HI
+    // document.querySelector(".ml8").classList.remove('hide');
+    let hiAnimation = anime.timeline({loop: false}).add({
+      targets: '.ml8 .circle-white',
+      scale: [0, 3],
+      opacity: [1, 0],
+      easing: "easeInOutExpo",
+      rotateZ: 360,
+      duration: 1100
+    })
+    .add({
+      targets: '.ml8 .circle-container',
+      scale: [0, 1],
+      duration: 1100,
+      easing: "easeInOutExpo",
+      offset: '-=1000'
+    })
+    .add({
+      targets: '.ml8 .circle-dark',
+      scale: [0, 1],
+      duration: 1100,
+      easing: "easeOutExpo",
+      offset: '-=600'
+    })
+    .add({
+      targets: '.ml8 .letters-left',
+      scale: [0, 1],
+      translateY: ['38%', '38%'], //centering letters
+      duration: 1200,
+      offset: '-=550'
+    })
+    .add({
+      targets: '.ml8 .bang',
+      scale: [0, 1],
+      translateY: ['38%', '38%'], //centering letters
+      opacity: [0,1],
+      rotateZ: [45, 15],
+      duration: 1200,
+      offset: '-=1000'
+    })
+    .add({}); // fixes "!" bug
 
-//     animations.push( headerFadeIn );
-//     animations.push( hiAnimation );
-//   };
-//   const runTextAnimation = () => {
-//     if (!animations.length) { 
-//       prepareAnimations();
-//     } else {
-//       animations.forEach( a => a.restart() );
-//     }
-//   };
+    animations.push( headerFadeIn );
+    animations.push( hiAnimation );
+  };
+  const runTextAnimation = () => {
+    if (!animations.length) { 
+      prepareAnimations();
+    } else {
+      animations.forEach( a => a.restart() );
+    }
+  };
 
   onMount(() => {
-    // runTextAnimation();
-    console.log('hello')
+    runTextAnimation();
   })
 </script>
 
@@ -186,6 +184,12 @@ import { onMount } from "svelte";
           animation: rotate360 8s linear infinite;
 }
 
+h1.header {
+  text-align: left;
+  letter-spacing: 2px;
+  line-height: 1.2em;
+  margin: 0;
+}
 
 @-webkit-keyframes rotate360 {
   from {
